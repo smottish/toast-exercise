daa670cc4b7cdf8dccd3e1d591e7a6334a04ea42
Shane Mottishaw <shane@automottic.ca>
2023-03-12 15:07:02 -0700

    WIP: Add scaffolding for display toast on new submission
    - The "Like" button doesn't work yet
    - There's a memory leak if Content ever unmounts (other than when the page reloads).
      See the comment in the first useEffect in Content for details.
    - Keeping everything in Content for now. If there's time and it makes sense I'll
      decompose components into separate modules.
    
    Known issue: if a new form submission is submitted while a toast is displayed for a
    previous one, the previous one will be replaced and the customer won't be able to
    like it. Discuss with the team how best to handle this scenario. E.g. we could display
    one toast for each submission, but then the screen could be cluttered with many toasts.

--- src/Content.js
+++ src/Content.js
@@ -1,8 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
+import Snackbar from '@mui/material/Snackbar';
 import Typography from '@mui/material/Typography';
+import { onMessage } from './service/mockServer';
+
+function getNewFormSubmissionMessage(formSubmission) {
+  if (!formSubmission || !formSubmission.data) {
+    return ""
+  }
+
+  const firstName = formSubmission.data.firstName;
+  const lastName = formSubmission.data.lastName;
+  const email = formSubmission.data.email;
+  return `${firstName} ${lastName} ${email}`
+}
 
 export default function Content() {
+  const [newFormSubmission, setNewFormSubmission] = useState({});
+  const [displayToast, setDisplayToast] = useState(false);
+
+  useEffect(() => {
+    function onNewFormSubmission(formSubmission) {
+      setNewFormSubmission(formSubmission);
+    }
+
+    onMessage(onNewFormSubmission)
+    // If mockServer had a method to unregister a callback, I'd
+    // do that here in a useEffect cleanup function to avoid
+    // a potential memory leak on component unmount/remount. This
+    // component should only unmount/mount on page (re)load, so
+    // there shouldn't be a memory leak in this case but it would
+    // still be good practice to unregister the callback.
+  }, []);
+
+  useEffect(() => {
+    if (newFormSubmission.id === undefined) {
+      return;
+    }
+
+    setDisplayToast(true);
+  }, [newFormSubmission.id]);
+
+  function handleCloseToast(ev, reason) {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setDisplayToast(false);
+    setNewFormSubmission({});
+  }
+
+  function onLikeNewSubmission() {
+    // TODO: implement
+  }
+
+  const toastAction = (
+    <React.Fragment>
+      <Button onClick={onLikeNewSubmission}>Like</Button>
+      <IconButton onClick={handleCloseToast}>
+        <CloseIcon />
+      </IconButton>
+    </React.Fragment>
+  );
+
   return (
     <Box sx={{marginTop: 3}}>
       <Typography variant="h4">Liked Form Submissions</Typography>
@@ -10,6 +73,16 @@ export default function Content() {
       <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
         TODO: List of liked submissions here (delete this line)
       </Typography>
+      <Snackbar
+        open={displayToast}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right'
+        }}
+        message={getNewFormSubmissionMessage(newFormSubmission)}
+        onClose={handleCloseToast}
+        action={toastAction}
+      />
     </Box>
   );
 }
